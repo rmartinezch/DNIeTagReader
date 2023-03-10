0602dcc7073cf9011a506272ee87bcab1896642e
Ronald Martinez <rmartinezch@gmail.com>
2023-03-10 17:17:16 -0500

    Initial interface

--- DNIeTagReader/ViewController.swift
+++ DNIeTagReader/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TextView: UITextView!
+    
+    @IBOutlet weak var TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func setText(_ sender: UIButton) {
+        print("set text !!!")
+        let mText = TextField.text
+        TextView.text = mText
+    }
+    
+    @IBAction func appendText(_ sender: UIButton) {
+        print("append text !!!")
+        let mText = TextField.text
+        TextView.text += mText ?? ""
+    }
 }
-
